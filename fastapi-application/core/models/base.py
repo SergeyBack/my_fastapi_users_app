27273264ccd442e1257e582136c6d0bb9b225055
SergeyManuylov <sergomanuilov@gmail.com>
2025-06-14 13:07:49 +0300

    create int id pk

--- fastapi-application/core/models/base.py
+++ fastapi-application/core/models/base.py
@@ -1,4 +1,4 @@
-from sqlalchemy.orm import DeclarativeBase,  Mapped, mapped_column, declared_attr
+from sqlalchemy.orm import DeclarativeBase, declared_attr
 from sqlalchemy import MetaData
 from core.config import settings
 
@@ -6,16 +6,13 @@ from core.config import settings
 class Base(DeclarativeBase):
     __abstract__ = True
     
-    
-    
     metadata = MetaData(
             naming_convention=settings.db.naming_convention,
     )
     
+    
     @declared_attr.directive
     def __tablename__(cls)->str:
             return f"{cls.__name__.lower()}s"
-    
-    id: Mapped[int] = mapped_column(primary_key=True)
-    
+
     
