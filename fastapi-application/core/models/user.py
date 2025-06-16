5060424b72e8c3d12f6319152035bd1e2a2ccb8d
SergeyManuylov <sergomanuilov@gmail.com>
2025-06-16 22:46:05 +0300

    corrected User int to UserIDtype

--- fastapi-application/core/models/user.py
+++ fastapi-application/core/models/user.py
@@ -3,16 +3,17 @@ from typing import TYPE_CHECKING
 from .base import Base
 from .mixins.int_id_pk import IntIdPkMixin
 
-from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
+from fastapi_users_db_sqlalchemy import (
+    SQLAlchemyBaseUserTable,
+    SQLAlchemyUserDatabase
+    )
 
 from core.types.user_id import UserIdType
 
-
-if TYPE_CHECKING:
-    from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.ext.asyncio import AsyncSession
 
 
-class User(Base, IntIdPkMixin, SQLAlchemyBaseUserTable[int]):
+class User(Base, IntIdPkMixin, SQLAlchemyBaseUserTable[UserIdType]):
     
     @classmethod
     def get_db(cls, session: AsyncSession):
