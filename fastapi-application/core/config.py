4ca057d3a93d5bb2bfbbc18a10f5e439cb105a54
SergeyManuylov <sergomanuilov@gmail.com>
2025-06-17 21:26:33 +0300

    update config add secrets

--- fastapi-application/core/config.py
+++ fastapi-application/core/config.py
@@ -36,6 +36,9 @@ class DatabaseConfig(BaseModel):
 
 class AccessToken(BaseModel):
     lifetime_seconds: int = 3600
+    reset_password_token_secret: str
+    verification_token_secret: str
+    
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
@@ -47,7 +50,7 @@ class Settings(BaseSettings):
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
     db: DatabaseConfig 
-    access_token: AccessToken= AccessToken()
+    access_token: AccessToken
     
     
 settings =Settings()  # type: ignore
