65257e208cf7163794f52688f9ecdcdbd2d69b4b
SergeyManuylov <sergomanuilov@gmail.com>
2025-07-03 16:59:21 +0300

    add taskiq broker

---
+++ fastapi-application/core/taskiq_broker.py
@@ -0,0 +1,18 @@
+__all__ = ("broker",)
+import logging
+
+from taskiq import TaskiqEvents, TaskiqState
+from taskiq_aio_pika import AioPikaBroker 
+from core.config import settings
+
+broker =AioPikaBroker(
+    url=settings.taskiq.url,
+)
+
+@broker.on_event(TaskiqEvents.WORKER_STARTUP)
+async def startup(state: TaskiqState) -> None:
+    logging.basicConfig(
+        level=settings.logging.log_level_value,
+        format=settings.taskiq.log_format,
+        datefmt=settings.logging.log_date_format,
+    )
