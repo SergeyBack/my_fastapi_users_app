75cb5b9b9995955227fa248a391317fd8c679f6d
SergeyManuylov <sergomanuilov@gmail.com>
2025-07-03 16:56:51 +0300

    fix users routes for fastapi users

--- fastapi-application/api/api_v1/users.py
+++ fastapi-application/api/api_v1/users.py
@@ -2,20 +2,7 @@ from fastapi import APIRouter
 from core.config import settings
 from core.schemas.user import UserRead, UserUpdate
 
-from typing import Annotated
-
-from fastapi import APIRouter, Depends
-
-from sqlalchemy.ext.asyncio import AsyncSession
-
-from core.models import db_helper
-from core.schemas.user import UserRead, UserCreate
-
-from crud import users as users_crud
-
 from .fastapi_users import fastapi_users
-from api.dependencies.authentication import authentication_backend
-
 
 router = APIRouter(
     prefix=settings.api.v1.users,
@@ -30,28 +17,42 @@ router.include_router(
         ),
 )
 
+# from typing import Annotated
 
-@router.get("", response_model = list[UserRead])
-async def get_users(
-    session: Annotated[
-        AsyncSession, 
-        Depends(db_helper.session_getter)
-        ],
-):
-    users = await users_crud.get_all_users(session=session)
-    return users
+# from fastapi import APIRouter, Depends
+
+# from sqlalchemy.ext.asyncio import AsyncSession
+
+# from core.models import db_helper
+# from core.schemas.user import UserRead, UserCreate
+
+# from crud import users as users_crud
+# from api.dependencies.authentication import authentication_backend
 
 
-@router.post("", response_model=UserRead)
-async def create_user(
-    session: Annotated[
-        AsyncSession,
-        Depends(db_helper.session_getter),
-    ],
-    user_create: UserCreate,
-):
-    user = await users_crud.create_user(
-        session=session,
-        user_create=user_create,
-    )
-    return user
+
+# @router.get("", response_model = list[UserRead])
+# async def get_users(
+#     session: Annotated[
+#         AsyncSession, 
+#         Depends(db_helper.session_getter)
+#         ],
+# ):
+#     users = await users_crud.get_all_users(session=session)
+#     return users
+
+
+# @router.post("", response_model=UserRead)
+# async def create_user(
+#     session: Annotated[
+#         AsyncSession,
+#         Depends(db_helper.session_getter),
+#     ],
+#     user_create: UserCreate,
+# ):
+#     user = await users_crud.create_user(
+#         session=session,
+#         user_create=user_create,
+#     )
+#     
+#     return user
